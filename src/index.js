365d5843f9396cc31c3ef9291dadbb5d7f3ea1ee
mashimeng <jphunt@163.com>
2017-03-10 14:39:01 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import App from './components/app';
+
+ReactDom.render(
+    <App/>,
+    document.getElementById('root')
+);
